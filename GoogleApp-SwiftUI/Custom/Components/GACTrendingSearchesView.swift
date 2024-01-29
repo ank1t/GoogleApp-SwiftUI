dccc27fd8e8a8431c17edc02a771fb999318d3d4
Ankit Singh <159.ank159@gmail.com>
2024-01-28 20:38:35 -0500

    Finalizing GACTrendingSearchesView

--- GoogleApp-SwiftUI/Custom/Components/GACTrendingSearchesView.swift
+++ GoogleApp-SwiftUI/Custom/Components/GACTrendingSearchesView.swift
@@ -5,4 +5,30 @@
 //  Created by Singh,Ankit on 2024-01-28.
 //
 
-import Foundation
+import SwiftUI
+
+struct GACTrendingSearchesView: View {
+    var trendingSearchText: String = "Some text"
+    
+    var body: some View {
+        HStack {
+            ZStack {
+                Circle()
+                    .fill(Color(LightTheme.gray400))
+                    
+                Image(for: .trending)
+                    .renderAsResizable(.fit, true)
+                    .frame(width: Dimensions.FrameSize.size12)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, Dimensions.Padding.padding10)
+            }
+            .frame(width: Dimensions.FrameSize.size35)
+            .padding(.leading, Dimensions.Padding.padding15)
+            .padding(.trailing, Dimensions.Padding.padding10)
+            
+            Text(trendingSearchText)
+                .foregroundColor(.white)
+                .font(.system(size: Dimensions.FontSize.font18))
+        }
+    }
+}
--- GoogleApp-SwiftUI/Views/TrendingSearchesView.swift
+++ GoogleApp-SwiftUI/Views/TrendingSearchesView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct TrendingSearchesView: View {
+    private let trendingSearches = [
+        "valentine dinner ikea",
+        "disability benefits",
+        "nfl ravens chiefs",
+        "unrwa israel gaza",
+        "edmonton downtown farmers market",
+        "yellowstone new episodes",
+        "american airlines flight 271",
+        "nfl super bowl",
+        "daily horoscope today",
+        "weather forecast snow storm"
+    ]
+    
     var body: some View {
-        VStack {
-            GACSearchTextField()
-            Spacer()
+        ZStack {
+            LightTheme.tabBarBGColor.ignoresSafeArea()
+            VStack(alignment: .leading) {
+                GACSearchTextField()
+                Text("What's Trending")
+                    .padding(.bottom, Dimensions.Padding.padding20)
+                    .padding(.horizontal, Dimensions.Padding.padding15)
+                    .foregroundColor(.gray)
+                    .font(.system(size: Dimensions.FontSize.font16))
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        ForEach(trendingSearches, id: \.self) { search in
+                            GACTrendingSearchesView(trendingSearchText: search)
+                        }
+                    }
+                }
+                .scrollDisabled(true)
+                Spacer()
+            }
+            .ignoresSafeArea()
         }
-        .ignoresSafeArea()
+        
     }
 }
