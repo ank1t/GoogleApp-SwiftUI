33ab02772013f9579f584ce99bd240a06dabad4e
Ankit Singh <159.ank159@gmail.com>
2024-01-28 21:09:36 -0500

    Customizing trending searches view for different use cases

--- GoogleApp-SwiftUI/Custom/Components/GACSearchTextField.swift
+++ GoogleApp-SwiftUI/Custom/Components/GACSearchTextField.swift
@@ -7,21 +7,56 @@
 
 import SwiftUI
 
+enum TextfieldAppearence {
+    case homepageLook
+    case trendingPageLook
+    
+    var searchFieldText: String {
+        self == .homepageLook ? Constants.searchTextfieldPlaceholder : Constants.searchOrURLTextfieldPlaceholder
+    }
+    
+    var fontSize: CGFloat {
+        self == .homepageLook ? Dimensions.FontSize.font24 : Dimensions.FontSize.font16
+    }
+    
+    var placeholderColor: Color {
+        self == .homepageLook ? .white : .gray
+    }
+    
+    var hPadding: CGFloat {
+        self == .homepageLook ? Dimensions.Padding.padding24 : Dimensions.Padding.padding50
+    }
+}
+
 struct GACSearchTextField: View {
     @State var searchQuery: String = ""
+    let appearence: TextfieldAppearence
     
     var body: some View {
         ZStack {
-            TextField("", text: $searchQuery, prompt: Text(Constants.searchTextfieldPlaceholder).foregroundColor(Color(.white)))
+            TextField("",
+                      text: $searchQuery,
+                      prompt: Text(appearence.searchFieldText).foregroundColor(appearence.placeholderColor))
                 .padding(.vertical)
-                .padding(.horizontal, Dimensions.Padding.padding24)
+                .padding(.horizontal, appearence.hPadding)
                 .background(Color(LightTheme.gray400))
                 .clipShape(Capsule(style: .continuous))
-                .font(.system(size: Dimensions.FontSize.font24))
+                .font(.system(size: appearence.fontSize))
                 .foregroundColor(Color(.white))
                 .padding()
             
             HStack {
+                if appearence == .trendingPageLook {
+                    Image(for: .backChevron)
+                        .renderAsResizable(.fit)
+                        .foregroundColor(.white)
+                        .frame(width: Dimensions.FrameSize.size15, height: Dimensions.FrameSize.size15)
+                        .padding(.leading, Dimensions.Padding.padding32)
+                        .onTapGesture {
+                            
+                        }
+                }
+                
                 Spacer()
                 Image(for: .mic)
                     .renderAsResizable(.fit)
