2b28946c1fe2325edbd7022b8cb38e4f40ac4b77
Ankit Singh <159.ank159@gmail.com>
2024-02-04 19:51:52 -0500

    Positioned close icon correctly

--- GoogleApp-SwiftUI/Custom/Components/GACArticleDetailTopBar.swift
+++ GoogleApp-SwiftUI/Custom/Components/GACArticleDetailTopBar.swift
@@ -10,6 +10,21 @@ import SwiftUI
 
 struct GACArticleDetailTopBar: View {
     var body: some View {
-        Text("Top bar")
+        ZStack {
+            LightTheme.tabBarBGColor
+                .ignoresSafeArea(edges: .top)
+            
+            HStack(spacing: Dimensions.Spacing.spacing5) {
+                Image(for: .close)
+                    .renderAsResizable(.fit)
+                    .foregroundColor(.white)
+                    .frame(width: Dimensions.FrameSize.size15,
+                           height: Dimensions.FrameSize.size15)
+                    .padding(.leading, Dimensions.Padding.padding12)
+                Spacer()
+            }
+            
+        }
+        .frame(height: Dimensions.FrameSize.size30)
     }
 }
