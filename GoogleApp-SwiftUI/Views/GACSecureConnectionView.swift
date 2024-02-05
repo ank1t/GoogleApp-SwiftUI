2fd088e614b84f499fe3e5faa2295cca6ee09af2
Ankit Singh <159.ank159@gmail.com>
2024-02-04 19:54:23 -0500

    Adding secure connection view

---
+++ GoogleApp-SwiftUI/Views/GACSecureConnectionView.swift
@@ -0,0 +1,16 @@
+//
+//  GACSecureConnectionView.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-02-04.
+//
+
+import Foundation
+import SwiftUI
+
+struct GACSecureConnectionView: View{
+    
+    var body: some View {
+        Text("Secure connection view")
+    }
+}
