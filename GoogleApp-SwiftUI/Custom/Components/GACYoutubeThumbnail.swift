bc47b580682409cdfdf3d096655d599043f757cf
Ankit Singh <159.ank159@gmail.com>
2024-02-06 19:27:23 -0500

    Adding GACYoutubeThumbnail view

---
+++ GoogleApp-SwiftUI/Custom/Components/GACYoutubeThumbnail.swift
@@ -0,0 +1,17 @@
+//
+//  GACWebview.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-02-05
+//
+
+import Foundation
+import SwiftUI
+
+struct GACWebview: View {
+    let urlStr: String
+    
+    var body: some View {
+        Text("")
+    }
+}
---
+++ GoogleApp-SwiftUI/Custom/UIKit views/GACWebview.swift
@@ -0,0 +1,28 @@
+//
+//  GACWebview.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-02-06
+//
+        
+import Foundation
+import WebKit
+import SwiftUI
+
+struct GACWebviewWrapper: UIViewRepresentable {
+    @YoutubeURL var videoID: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        guard let url = URL(string: videoID) else {
+            return webView
+        }
+        webView.scrollView.isScrollEnabled = false
+        //        webView.load(URLRequest(url: url))
+        return webView
+    }
+    
+    func updateUIView(_ view: WKWebView, context: Context) {
+        
+    }
+}
