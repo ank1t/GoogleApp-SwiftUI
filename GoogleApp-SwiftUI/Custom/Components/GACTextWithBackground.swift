0b0f3f977f6f85bc9674f92d45322842dfdc736a
Ankit Singh <159.ank159@gmail.com>
2024-02-07 21:42:15 -0500

    Creating GACTextWithBackground component

--- GoogleApp-SwiftUI/Custom/Components/GACTextWithBackground.swift
+++ GoogleApp-SwiftUI/Custom/Components/GACTextWithBackground.swift
@@ -5,5 +5,25 @@
 //  Created by Singh, Ankit on 2024-02-07
 //
         
-
 import Foundation
+import SwiftUI
+
+struct GACTextWithBackground<Content: Shape>: View {
+    let text: String
+    let fontSize: CGFloat
+    let textColor: Color
+    let horizontalPadding: CGFloat
+    let verticalPadding: CGFloat
+    let backgroundColor: Color
+    let Shape: Content
+    
+    var body: some View {
+        Text(text)
+            .font(.system(size: fontSize))
+            .foregroundColor(textColor)
+            .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, verticalPadding)
+            .background(backgroundColor)
+            .clipShape(Shape)
+    }
+}
