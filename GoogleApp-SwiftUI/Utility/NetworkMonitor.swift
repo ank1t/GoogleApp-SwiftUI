31e69e32b552cb5b1de25ee95cd67f237bbcaea7
Ankit Singh <159.ank159@gmail.com>
2024-02-07 21:14:44 -0500

    Adding Network monitor code

--- GoogleApp-SwiftUI/Utility/NetworkMonitor.swift
+++ GoogleApp-SwiftUI/Utility/NetworkMonitor.swift
@@ -6,4 +6,32 @@
 //
         
 
-import Foundation
+import Network
+import SwiftUI
+
+enum NetworkStatus: String {
+    case connected
+    case disconnected
+}
+
+class Monitor: ObservableObject {
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "Monitor")
+    
+    @Published var status: NetworkStatus = .connected
+    
+    init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                if path.status == .satisfied {
+                    self.status = .connected
+                    
+                } else {
+                    self.status = .disconnected
+                }
+            }
+        }
+        monitor.start(queue: queue)
+    }
+}
