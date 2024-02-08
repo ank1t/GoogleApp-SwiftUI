2b27329f48b97bd01997e2d5008c40efc015d747
Ankit Singh <159.ank159@gmail.com>
2024-02-07 21:41:45 -0500

    Adding GACNoNetworkConnection component

--- GoogleApp-SwiftUI/Custom/Components/GACNoNetworkConnection.swift
+++ GoogleApp-SwiftUI/Custom/Components/GACNoNetworkConnection.swift
@@ -5,5 +5,26 @@
 //  Created by Singh, Ankit on 2024-02-07
 //
         
+import SwiftUI
 
-import Foundation
+struct GACNoNetworkConnection: View {
+    var body: some View {
+        VStack(spacing: Dimensions.Spacing.spacing25) {
+            Spacer()
+            Image(for: .noNetwork)
+                .renderAsResizable(.fit)
+                .frame(height: Dimensions.FrameSize.size50)
+            
+            Text("No network connection")
+                .foregroundColor(.white)
+            
+            Button(action: {}) {
+                GACTextWithBackground(text: "Try again", fontSize: Dimensions.FontSize.font14,
+                                      textColor: .black, horizontalPadding: Dimensions.Padding.padding12,
+                                      verticalPadding: Dimensions.Padding.padding8, backgroundColor: .teal,
+                                      Shape: Capsule())
+            }
+            Spacer()
+        }
+    }
+}
