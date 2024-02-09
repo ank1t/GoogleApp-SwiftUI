211897e9f1f75136eaa2a2ad251a3434c0d8466b
Ankit Singh <159.ank159@gmail.com>
2024-02-08 21:11:40 -0500

    GACTextConfig - ongoing

--- GoogleApp-SwiftUI/Models/GACTextConfig.swift
+++ GoogleApp-SwiftUI/Models/GACTextConfig.swift
@@ -5,5 +5,11 @@
 //  Created by Singh, Ankit on 2024-02-08
 //
         
-
 import Foundation
+import SwiftUI
+
+struct GACTextConfig {
+    let text: String
+    let textColor: Color
+    let textFont: Font
+}
