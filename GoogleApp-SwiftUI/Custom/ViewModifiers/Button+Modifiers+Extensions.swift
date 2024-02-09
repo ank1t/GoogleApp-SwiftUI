2ca1dfb721af48134f6de2619069a73ac8c618eb
Ankit Singh <159.ank159@gmail.com>
2024-02-08 21:28:27 -0500

    Adding Button+Modifier+extensions to simplify code

--- GoogleApp-SwiftUI/Custom/ViewModifiers/Button+Modifiers+Extensions.swift
+++ GoogleApp-SwiftUI/Custom/ViewModifiers/Button+Modifiers+Extensions.swift
@@ -5,5 +5,21 @@
 //  Created by Singh, Ankit on 2024-02-08
 //
         
-
 import Foundation
+import SwiftUI
+
+struct ButtonStyle: ViewModifier {
+    let config: GACButtonConfig
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(config.btnColor)
+            .font(config.btnFont)
+    }
+}
+
+extension Button {
+    func applyButtonStyle(_ config: GACButtonConfig) {
+        modifier(ButtonStyle(config: config))
+    }
+}
