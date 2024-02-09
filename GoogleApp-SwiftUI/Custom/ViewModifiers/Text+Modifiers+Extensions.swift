55d32380bc243fcd1564fb30c110425ba3861a74
Ankit Singh <159.ank159@gmail.com>
2024-02-08 21:25:13 -0500

    Adding text modifier and extensions to simplify usage

--- GoogleApp-SwiftUI/Custom/ViewModifiers/Text+Modifiers+Extensions.swift
+++ GoogleApp-SwiftUI/Custom/ViewModifiers/Text+Modifiers+Extensions.swift
@@ -5,5 +5,21 @@
 //  Created by Singh, Ankit on 2024-02-08
 //
         
-
 import Foundation
+import SwiftUI
+
+struct TextStyle: ViewModifier {
+    let config: GACTextConfig
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(config.textColor)
+            .font(config.textFont)
+    }
+}
+
+extension Text {
+    func applyTextStyle(_ config: GACTextConfig) {
+        modifier(TextStyle(config: config))
+    }
+}
