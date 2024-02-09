bdb37c70a377fcaeaab99b7a728064b9401ecbd6
Ankit Singh <159.ank159@gmail.com>
2024-02-08 21:14:46 -0500

    GACButtonConfig - ongoing

--- GoogleApp-SwiftUI/Models/GACButtonConfig.swift
+++ GoogleApp-SwiftUI/Models/GACButtonConfig.swift
@@ -5,5 +5,11 @@
 //  Created by Singh, Ankit on 2024-02-08
 //
         
-
 import Foundation
+import SwiftUI
+
+struct GACButtonConfig {
+    let text: String
+    let textFont: Font
+    let textColor: Color
+}
