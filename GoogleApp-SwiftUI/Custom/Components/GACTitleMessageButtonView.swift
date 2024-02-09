2478d7c0dc8f20dbe7e0f53f3c6c68bf79c2e344
Ankit Singh <159.ank159@gmail.com>
2024-02-08 21:15:15 -0500

    GACTitleMessageButtonView - ongoing

--- GoogleApp-SwiftUI/Custom/Components/GACTitleMessageButtonView.swift
+++ GoogleApp-SwiftUI/Custom/Components/GACTitleMessageButtonView.swift
@@ -9,8 +9,9 @@ import Foundation
 import SwiftUI
 
 struct GACTitleMessageButtonView: View {
-    let title: String
-    let titleFont
+    let titleConfig: GACTextConfig
+    let messageConfig: GACTextConfig
+    let buttonConfig: GACButtonConfig
     var body: some View {
         ZStack {
             VStack {
