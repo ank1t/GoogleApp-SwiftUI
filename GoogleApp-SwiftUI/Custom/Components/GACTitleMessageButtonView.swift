7d1e36145f08d5ddb6f12434949105d8648b465f
Ankit Singh <159.ank159@gmail.com>
2024-02-08 21:08:59 -0500

    GACTitleMessageButtonView ongoing

--- GoogleApp-SwiftUI/Custom/Components/GACTitleMessageButtonView.swift
+++ GoogleApp-SwiftUI/Custom/Components/GACTitleMessageButtonView.swift
@@ -5,5 +5,18 @@
 //  Created by Singh, Ankit on 2024-02-08
 //
         
-
 import Foundation
+import SwiftUI
+
+struct GACTitleMessageButtonView: View {
+    let title: String
+    let titleFont
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("You are offline")
+                    
+            }
+        }
+    }
+}
