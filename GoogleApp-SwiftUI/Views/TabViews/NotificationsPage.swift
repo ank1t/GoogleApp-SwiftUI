08819205ca88a05dfe2e545aa9f511f170fa4305
Ankit Singh <159.ank159@gmail.com>
2024-02-12 20:01:21 -0500

    Adding pages for tabs

---
+++ GoogleApp-SwiftUI/Views/TabViews/NotificationsPage.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationsPage.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-02-13
+//
+        
+
+import Foundation
---
+++ GoogleApp-SwiftUI/Views/TabViews/TabsPage.swift
@@ -0,0 +1,9 @@
+//
+//  TabsPage.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-02-13
+//
+        
+
+import Foundation
