f16ad6edff333a741feeefbc82270d69ee36e3b0
Ankit Singh <159.ank159@gmail.com>
2024-02-15 20:58:27 -0500

    Tab page search results ongoing

--- GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/GACTabPageSearchResult.swift
+++ GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/GACTabPageSearchResult.swift
@@ -9,10 +9,26 @@ import Foundation
 import SwiftUI
 
 struct GACTabPageSearchResult: View {
+    let date: String
+    let url: String
+    
+    var subtitle: String {
+        date + Constants.bulletPoint + url
+    }
+    
     var body: some View {
         HStack(spacing: Dimensions.Spacing.spacing10) {
             Image(for: .noNetwork)
-                .
+            
+            VStack(spacing: Dimensions.Spacing.spacing5, alignment: .leading) {
+                Text("The Mysterious prehistoric undercaves")
+                    .font(.system(size: Dimensions.FontSize.font14))
+                    .foregroundColor(.white)
+                
+                Text(subtitle)
+                    .font(.system(size: Dimensions.FontSize.font10))
+                    .foregroundColor(.gray)
+            }
         }
     }
 }
