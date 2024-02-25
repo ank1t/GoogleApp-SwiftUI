6979a8cd3006cfd3121c382660ef8e8a149734ff
Ankit Singh <159.ank159@gmail.com>
2024-02-24 21:28:12 -0500

    Adding offset reporting scrollView

---
+++ GoogleApp-SwiftUI/Custom/Components/Common_Components/OffsetObservingScrollView.swift
@@ -0,0 +1,36 @@
+//
+//  File2.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-02-23
+//
+        
+import Foundation
+import SwiftUI
+
+struct OffsetObservingScrollView<Content: View>: View {
+    var axes: Axis.Set = [.vertical]
+    var showsIndicators = true
+    @Binding var offset: CGPoint
+    @ViewBuilder var content: () -> Content
+
+    private let coordinateSpaceName = UUID()
+    var body: some View {
+        ScrollView(axes, showsIndicators: showsIndicators) {
+            PositionObservingView(
+                coordinateSpace: .named(coordinateSpaceName),
+                position: Binding(
+                    get: { offset },
+                    set: { newOffset in
+                        offset = CGPoint(
+                            x: -newOffset.x,
+                            y: -newOffset.y
+                        )
+                    }
+                ),
+                content: content
+            )
+        }
+        .coordinateSpace(name: coordinateSpaceName)
+    }
+}
