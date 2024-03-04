c02e08b46af71d6d122d8c5e2e20bdc8451a6db2
Ankit Singh <159.ank159@gmail.com>
2024-03-03 21:44:41 -0500

    Adding RoundedCorners functionality

--- GoogleApp-SwiftUI/Custom/Components/Common_Components/RoundedCorners.swift
+++ GoogleApp-SwiftUI/Custom/Components/Common_Components/RoundedCorners.swift
@@ -5,5 +5,15 @@
 //  Created by Singh, Ankit on 2024-03-03
 //
         
-
 import Foundation
+import SwiftUI
+
+struct RoundedCorners: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
