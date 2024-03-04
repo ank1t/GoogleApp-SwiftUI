c960b589d983059d620d3b20da1ca76c5712e891
Ankit Singh <159.ank159@gmail.com>
2024-03-03 21:43:42 -0500

    Bottom DrawerSheet - ongoing

--- GoogleApp-SwiftUI/Extensions/UIView+Extensions.swift
+++ GoogleApp-SwiftUI/Extensions/UIView+Extensions.swift
@@ -5,5 +5,11 @@
 //  Created by Singh, Ankit on 2024-03-03
 //
         
-
 import Foundation
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorners(radius: radius, corners: corners))
+    }
+}
--- GoogleApp-SwiftUI/Models/BottomSheetDrawerConfig.swift
+++ GoogleApp-SwiftUI/Models/BottomSheetDrawerConfig.swift
@@ -5,5 +5,12 @@
 //  Created by Singh, Ankit on 2024-03-03
 //
         
-
 import Foundation
+import SwiftUI
+
+struct BottomSheetDrawerConfig {
+    let image: Utility.ImageName = .saveHistory
+    let title: String = "Your history isn't being saved to your account"
+    let subtitle: String = "Your Discover feed isn't based on your history because your Web & App Activity setting is off"
+    let colorPairs: (Color, Color) = (LightTheme.tabBarBGColor, .blue)
+}
