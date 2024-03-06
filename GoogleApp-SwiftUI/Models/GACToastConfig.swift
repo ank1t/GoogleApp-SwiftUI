741d7cc8b510018d7f7a0a31ea485bb661d72701
Ankit Singh <159.ank159@gmail.com>
2024-03-05 21:15:33 -0500

    Finalizing GACToastConfig implementation

--- GoogleApp-SwiftUI/Models/GACToastConfig.swift
+++ GoogleApp-SwiftUI/Models/GACToastConfig.swift
@@ -9,10 +9,31 @@ import Foundation
 import SwiftUI
 
 enum ToastStyle {
+    case error
+    case warning
+    case success
+    case info
     
+    var highlightColor: Color {
+        switch self {
+            case .error: .red
+            case .warning: .yellow
+            case .success: .green
+            case .info: .clear
+        }
+    }
+    
+    var icon: String {
+        switch self {
+            case .error: "info.circle.fill"
+            case .warning: "exclamationmark.triangle.fill"
+            case .success: "checkmark.circle.fill"
+            case .info: "xmark.circle.fill"
+        }
+    }
 }
 
-struct ToastConfig: View {
+struct GACToastConfig: View {
     var style: ToastStyle
     var message: String
     var duration: Double = 0.3
