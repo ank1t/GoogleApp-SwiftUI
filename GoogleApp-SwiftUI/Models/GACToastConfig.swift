39930c718cbf676f29abd75abd265bf254ee1db9
Ankit Singh <159.ank159@gmail.com>
2024-03-05 21:07:02 -0500

    Toast config - ongoing

--- GoogleApp-SwiftUI/Models/GACToastConfig.swift
+++ GoogleApp-SwiftUI/Models/GACToastConfig.swift
@@ -5,5 +5,15 @@
 //  Created by Singh, Ankit on 2024-03-05
 //
         
-
 import Foundation
+import SwiftUI
+
+enum ToastStyle {
+    
+}
+
+struct ToastConfig: View {
+    var style: ToastStyle
+    var message: String
+    var duration: Double = 0.3
+}
