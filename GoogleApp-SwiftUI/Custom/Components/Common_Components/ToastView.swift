995cdcd1f73c125e9d1f7247ebe78c58f202d34c
Ankit Singh <159.ank159@gmail.com>
2024-03-05 21:23:11 -0500

    ToastView - ongoing

--- GoogleApp-SwiftUI/Custom/Components/Common_Components/ToastView.swift
+++ GoogleApp-SwiftUI/Custom/Components/Common_Components/ToastView.swift
@@ -5,5 +5,22 @@
 //  Created by Singh, Ankit on 2024-03-04
 //
         
-
 import Foundation
+import SwiftUI
+
+struct ToastView: View {
+    let config: GACToastConfig
+    
+    var body: some View {
+        HStack(spacing: Dimensions.Spacing.spacing15) {
+            if config.style == .info,
+               let favIcon = config.lastVisitedPageFavIcon {
+                AsyncImage(url: URL(string: favIcon))
+                    .frame(width: Dimensions.FrameSize.size20,
+                           height: Dimensions.FrameSize.size20)
+            }
+            Text(config.message)
+                .applyTextStyle(GACTextConfig(text: <#T##String#>, textColor: <#T##Color#>, textFont: <#T##Font#>))
+        }
+    }
+}
