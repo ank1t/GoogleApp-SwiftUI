18db780c4111e4c1cc9b58db784240819437a916
Ankit Singh <159.ank159@gmail.com>
2024-03-06 20:50:49 -0500

    Adding file to hold toast modifiers and extensions

---
+++ GoogleApp-SwiftUI/Custom/ViewModifiers/Toast+Modifiers+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Toast+Modifiers+Extensions.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-03-06
+//
+        
+import Foundation
+import SwiftUI
+
+struct ToastModifier: ViewModifier {
+    
+}
