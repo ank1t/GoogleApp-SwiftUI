c513419c6f4be792c3f51ea21d306e3c257a17e7
Ankit Singh <159.ank159@gmail.com>
2024-03-09 10:30:32 -0500

    GACTabsSearchBar - ongoing

--- GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/GACTabsSearchBar.swift
+++ GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/GACTabsSearchBar.swift
@@ -10,23 +10,46 @@ import SwiftUI
 
 struct GACTabsSearchBarAndResults: View {
     @State var searchStr: String = ""
-    
+    @FocusState private var searchFieldIsFocused: Bool
+
     var body: some View {
         ZStack {
             LightTheme.tabBarBGColor
                 .ignoresSafeArea()
             VStack {
-                HStack(spacing: Dimensions.Spacing.spacing10) {
-                    Image(for: .backChevron)
-                        .foregroundColor(.gray)
+                HStack(spacing: Dimensions.Spacing.spacing20) {
+                    if searchFieldIsFocused {
+                        Image(for: .backChevron)
+                            .foregroundColor(.gray)
+                            .onTapGesture {
+                                withAnimation(.default) {
+                                    searchFieldIsFocused.toggle()
+                                }
+                            }
+                    }
                     HStack(spacing: Dimensions.Spacing.spacing10) {
                         Image(for: .searchRecent)
                             .foregroundColor(.gray)
-                        TextField("Search tabs, collections and recents", text: $searchStr)
+                        TextField("",
+                                  text: $searchStr,
+                                  prompt: Text("Search tabs, collections and recents").foregroundColor(.gray))
+                            .foregroundColor(.white)
+                            .focused($searchFieldIsFocused)
+                    }
+                    .padding(.vertical, Dimensions.Padding.padding10)
+                    .padding(.horizontal, Dimensions.Padding.padding15)
+                    .background(Color(LightTheme.gray400))
+                    .clipShape(Capsule(style: .continuous))
+                    if !searchFieldIsFocused {
+                        Image(for: .searchPrevious)
+                            .foregroundColor(.gray)
+                        Image(for: .more)
+                            .foregroundColor(.gray)
                     }
                 }
                 .padding(.vertical, Dimensions.Padding.padding10)
-                .padding(.horizontal, Dimensions.Padding.padding15)
+                .padding(.trailing, Dimensions.Padding.padding15)
+                .padding(.leading, Dimensions.Padding.padding20)
                 Spacer()
             }
         }
