9824ab71ed98cdc172ef61418cfefd95d02fcaf7
Ankit Singh <159.ank159@gmail.com>
2024-03-09 11:08:49 -0500

    Adding GACTabsSearchBar animation transition

--- GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/GACTabsSearchBar.swift
+++ GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/GACTabsSearchBar.swift
@@ -11,19 +11,20 @@ import SwiftUI
 struct GACTabsSearchBarAndResults: View {
     @State var searchStr: String = ""
     @FocusState private var searchFieldIsFocused: Bool
-
+    @State private var showingRecentActivity: Bool = false
+    
     var body: some View {
         ZStack {
             LightTheme.tabBarBGColor
                 .ignoresSafeArea()
             VStack {
                 HStack(spacing: Dimensions.Spacing.spacing20) {
-                    if searchFieldIsFocused {
+                    if showingRecentActivity {
                         Image(for: .backChevron)
                             .foregroundColor(.gray)
                             .onTapGesture {
                                 withAnimation(.default) {
-                                    searchFieldIsFocused.toggle()
+                                    searchFieldIsFocused = false
                                 }
                             }
                     }
@@ -35,12 +36,17 @@ struct GACTabsSearchBarAndResults: View {
                                   prompt: Text("Search tabs, collections and recents").foregroundColor(.gray))
                             .foregroundColor(.white)
                             .focused($searchFieldIsFocused)
+                            .onChange(of: searchFieldIsFocused) { newValue in
+                                withAnimation(.default) {
+                                    showingRecentActivity.toggle()
+                                }
+                            }
                     }
                     .padding(.vertical, Dimensions.Padding.padding10)
                     .padding(.horizontal, Dimensions.Padding.padding15)
                     .background(Color(LightTheme.gray400))
                     .clipShape(Capsule(style: .continuous))
-                    if !searchFieldIsFocused {
+                    if !showingRecentActivity {
                         Image(for: .searchPrevious)
                             .foregroundColor(.gray)
                         Image(for: .more)
