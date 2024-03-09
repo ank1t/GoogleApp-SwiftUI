f07c09f55a43c84ad5782571b6ce5a972627447d
Ankit Singh <159.ank159@gmail.com>
2024-03-09 18:40:10 -0500

    Adding placeholder images

---
+++ GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/GACSavedArticles.swift
@@ -0,0 +1,9 @@
+//
+//  GACSavedArticles.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-03-09
+//
+        
+
+import Foundation
---
+++ GoogleApp-SwiftUI/Models/GACSavedArticlesConfig.swift
@@ -0,0 +1,9 @@
+//
+//  GACSavedArticlesConfig.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-03-09
+//
+        
+
+import Foundation
