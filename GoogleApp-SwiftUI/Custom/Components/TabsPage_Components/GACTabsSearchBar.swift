028fe698439f2e3c54e657031054b636c6a6cd21
Ankit Singh <159.ank159@gmail.com>
2024-03-09 09:59:25 -0500

    GACTabsSearchbar - ongoing

--- GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/GACTabsSearchBar.swift
+++ GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/GACTabsSearchBar.swift
@@ -5,5 +5,30 @@
 //  Created by Singh, Ankit on 2024-03-09
 //
         
-
 import Foundation
+import SwiftUI
+
+struct GACTabsSearchBarAndResults: View {
+    @State var searchStr: String = ""
+    
+    var body: some View {
+        ZStack {
+            LightTheme.tabBarBGColor
+                .ignoresSafeArea()
+            VStack {
+                HStack(spacing: Dimensions.Spacing.spacing10) {
+                    Image(for: .backChevron)
+                        .foregroundColor(.gray)
+                    HStack(spacing: Dimensions.Spacing.spacing10) {
+                        Image(for: .searchRecent)
+                            .foregroundColor(.gray)
+                        TextField("Search tabs, collections and recents", text: $searchStr)
+                    }
+                }
+                .padding(.vertical, Dimensions.Padding.padding10)
+                .padding(.horizontal, Dimensions.Padding.padding15)
+                Spacer()
+            }
+        }
+    }
+}
