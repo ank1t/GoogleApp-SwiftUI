c84d2677481cb246af5f4eac83c628962479cca3
Ankit Singh <159.ank159@gmail.com>
2024-03-10 20:29:49 -0400

    Adding Animated Tabs

---
+++ GoogleApp-SwiftUI/Custom/Components/Common_Components/AnimatedTabs.swift
@@ -0,0 +1,15 @@
+//
+//  AnimatedTabs.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-03-10
+//
+        
+import Foundation
+import SwiftUI
+
+struct AnimatedTabs: View {
+    var body: some View {
+        
+    }
+}
