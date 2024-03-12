de3b19b6e10da4deeba49e676012188d97ccc98c
Ankit Singh <159.ank159@gmail.com>
2024-03-11 20:54:47 -0400

    Animated tabs - ongoing

--- GoogleApp-SwiftUI/ContentView.swift
+++ GoogleApp-SwiftUI/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HomePage()
+        AnimatedTabs(tabs: ["Tabs", "Saved", "Liked"])
     }
 }
 
--- GoogleApp-SwiftUI/Custom/Components/Common_Components/AnimatedTabs.swift
+++ GoogleApp-SwiftUI/Custom/Components/Common_Components/AnimatedTabs.swift
@@ -9,7 +9,18 @@ import Foundation
 import SwiftUI
 
 struct AnimatedTabs: View {
+    let tabs: [String]
+    
     var body: some View {
-        
+        ZStack {
+            Color.black
+            HStack(spacing: 0) {
+                ForEach(tabs, id: \.self) { text in
+                    Text(text)
+                        .foregroundColor(.white)
+                }
+            }
+        }
+        Spacer()
     }
 }
