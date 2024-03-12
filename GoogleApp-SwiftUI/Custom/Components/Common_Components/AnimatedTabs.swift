c415f3e8ce7258bf3fe4e0a939ef71c01305d2eb
Ankit Singh <159.ank159@gmail.com>
2024-03-11 21:10:17 -0400

    Animated tabs ongoing

--- GoogleApp-SwiftUI/Custom/Components/Common_Components/AnimatedTabs.swift
+++ GoogleApp-SwiftUI/Custom/Components/Common_Components/AnimatedTabs.swift
@@ -10,17 +10,25 @@ import SwiftUI
 
 struct AnimatedTabs: View {
     let tabs: [String]
+    @State private var selectedIndex: Int = 0
     
     var body: some View {
-        ZStack {
-            Color.black
-            HStack(spacing: 0) {
-                ForEach(tabs, id: \.self) { text in
-                    Text(text)
-                        .foregroundColor(.white)
+        GeometryReader { proxy in
+            ZStack {
+                Color.black
+                HStack(spacing: 0) {
+                    ForEach(Array(tabs.enumerated()), id: \.element) { index, text in
+                        Text(text)
+                            .foregroundColor(.white)
+                            .padding(.vertical, 8)
+                            .background(index == selectedIndex ? .green : .clear)
+                    }
+                    .frame(width: proxy.frame(in: .global).width/CGFloat(tabs.count))
                 }
             }
+            .frame(height: Dimensions.FrameSize.size25)
         }
+        
         Spacer()
     }
 }
