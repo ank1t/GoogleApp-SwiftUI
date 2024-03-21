c620f7d2c6a4f2babca1a32f607b74b50dd5dd2d
Ankit Singh <159.ank159@gmail.com>
2024-03-21 01:41:12 -0400

    Animated tabs - ongoing

--- GoogleApp-SwiftUI/Custom/Components/Common_Components/Animated Tabs/AnimatedTabs.swift
+++ GoogleApp-SwiftUI/Custom/Components/Common_Components/Animated Tabs/AnimatedTabs.swift
@@ -20,30 +20,4 @@ struct AnimatedTabs: View {
         .tabViewStyle(.page(indexDisplayMode: .never))
         .ignoresSafeArea()
     }
-    /*@State private var frameWidth: CGFloat = .zero
-    
-    var body: some View {
-        GeometryReader { proxy in
-            HStack(spacing: Dimensions.Spacing.spacing0) {
-                ForEach(Array(viewModel.tabs.enumerated()), id: \.element) { index, text in
-                    VStack(spacing: Dimensions.Spacing.spacing0) {
-                        Text(text)
-                            .foregroundColor(.white)
-                            .frame(width: proxy.size.width/CGFloat(viewModel.tabs.count))
-                            .padding(.vertical, Dimensions.Padding.padding15)
-                            .background(.black)
-                        
-                        Divider()
-                            .frame(width: proxy.size.width/CGFloat(viewModel.tabs.count))
-                            .background(selectedIndex == index ? .white : .clear)
-                    }
-                    .onTapGesture {
-                        selectedIndex = index
-                    }
-                    .animation(.spring(), value: selectedIndex)
-                }
-            }
-        }
-        Spacer()
-    }*/
 }
