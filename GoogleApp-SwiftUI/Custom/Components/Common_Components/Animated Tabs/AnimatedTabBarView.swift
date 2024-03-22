f8b832029361abeb9ed810a7d20bb98f47af00d1
Ankit Singh <159.ank159@gmail.com>
2024-03-22 02:47:59 -0400

    Animated tabs - ongoing

---
+++ GoogleApp-SwiftUI/Custom/Components/Common_Components/Animated Tabs/AnimatedTabBarView.swift
@@ -0,0 +1,15 @@
+//
+//  AnimatedTabBarView.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-03-20
+//
+        
+import Foundation
+import SwiftUI
+
+struct AnimatedTabBarView: View {
+    var body: some View {
+        
+    }
+}
---
+++ GoogleApp-SwiftUI/Custom/Components/Common_Components/Animated Tabs/AnimatedTabBarViewItem.swift
@@ -0,0 +1,27 @@
+//
+//  AnimatedTabBarViewItem.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-03-20
+//
+        
+import Foundation
+import SwiftUI
+
+struct AnimatedTabBarViewItem: View {
+    @Binding var selectedIndex: Int
+    
+    var itemName: String
+    var itemIndex: Int
+    
+    var body: some View {
+        Button {
+            selectedIndex = itemIndex
+        } label: {
+            VStack(spacing: Dimensions.Spacing.spacing0) {
+                Spacer()
+                Text(itemName)
+            }
+        }
+    }
+}
