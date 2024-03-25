96d302e6b2892a204d7570f6721cf6aa9fd22865
Ankit Singh <159.ank159@gmail.com>
2024-03-25 19:42:03 -0400

    Adding new article type

---
+++ GoogleApp-SwiftUI/Custom/Components/HomePage_Components/Articles/GACCarouselArticleView.swift
@@ -0,0 +1,15 @@
+//
+//  GACCarouselArticleView.swift
+//  GoogleApp-SwiftUI
+//
+//  Created by Singh, Ankit on 2024-03-25
+//
+        
+import Foundation
+import SwiftUI
+
+struct GACCarouselArticleView: View {
+    var body: some View {
+        
+    }
+}
