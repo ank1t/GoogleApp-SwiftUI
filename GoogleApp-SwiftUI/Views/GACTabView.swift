0b049770e2d1ad8cfefe3519395821c91cc57259
Ankit Singh <159.ank159@gmail.com>
2024-03-26 23:02:45 -0400

    App tab view - ongoing

--- GoogleApp-SwiftUI/Views/GACTabView.swift
+++ GoogleApp-SwiftUI/Views/GACTabView.swift
@@ -12,12 +12,16 @@ struct GACTabView: View {
     @State private var indexOfSelectedTab = 0
     
     var body: some View {
-        TabView(selection: $indexOfSelectedTab) {
-            HomePage().tag(0)
-            TabsPage().tag(1)
-            NotificationsPage().tag(2)
+        ZStack(alignment: .bottom) {
+            TabView(selection: $indexOfSelectedTab) {
+                HomePage().tag(0)
+                TabsPage().tag(1)
+                NotificationsPage().tag(2)
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .ignoresSafeArea()
+            
+            
         }
-        .tabViewStyle(.page(indexDisplayMode: .never))
-        .edgesIgnoringSafeArea(.all)
     }
 }
