72883a3b9a28430a5357b5b5cdf26799bf7a086b
Ankit Singh <159.ank159@gmail.com>
2024-03-29 21:30:10 -0400

    Saved tabs - ongoing

--- GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/Tabs/SavedTabs.swift
+++ GoogleApp-SwiftUI/Custom/Components/TabsPage_Components/Tabs/SavedTabs.swift
@@ -10,7 +10,19 @@ import SwiftUI
 
 struct SavedTabs: View {
     var body: some View {
-        LightTheme.tabBarBGColor
-            .ignoresSafeArea()
+        ZStack {
+            LightTheme.tabBarBGColor
+                .ignoresSafeArea()
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: Dimensions.Spacing.spacing10) {
+                    LightTheme.tabBarBGColor
+                        .frame(height: Dimensions.FrameSize.size60)
+                    Text("Recently saved")
+                    
+                    Spacer()
+                }
+            }
+        }
     }
 }
